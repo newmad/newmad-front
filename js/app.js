2f6df17b018e497303a4a75d03802e5d312fdb96
tinyjin <baram991103@gmail.com>
2018-09-02 07:57:05 +0900

    design weather icon

--- js/app.js
+++ js/app.js
@@ -12,6 +12,12 @@ import search from '../api/search';
 
 
 
+
+import thunderStorm from './weatherBG/thunderstorm';
+import rainy from './weatherBG/rainy';
+import snow from './weatherBG/snow';
+import mist from './weatherBG/mist';
+
 // 아이콘 이미지
 import icon0 from '../assets/images/icon-0.png'
 import icon1 from '../assets/images/icon-1.png'
@@ -28,12 +34,28 @@ import icon11 from '../assets/images/icon-11.png'
 import icon12 from '../assets/images/icon-12.png'
 import icon13 from '../assets/images/icon-13.png'
 
-import thunderStorm from './weatherBG/thunderstorm';
-import rainy from './weatherBG/rainy';
-import snow from './weatherBG/snow';
-import mist from './weatherBG/mist';
-
 const icons = [icon0, icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9, icon10, icon11, icon12, icon13];
+
+// 아이콘 이미지 화이트
+import iconWhite0 from '../assets/images/icon-white-0.png'
+import iconWhite1 from '../assets/images/icon-white-1.png'
+import iconWhite2 from '../assets/images/icon-white-2.png'
+import iconWhite3 from '../assets/images/icon-white-3.png'
+import iconWhite4 from '../assets/images/icon-white-4.png'
+import iconWhite5 from '../assets/images/icon-white-5.png'
+import iconWhite6 from '../assets/images/icon-white-6.png'
+import iconWhite7 from '../assets/images/icon-white-7.png'
+import iconWhite8 from '../assets/images/icon-white-8.png'
+import iconWhite9 from '../assets/images/icon-white-9.png'
+import iconWhite10 from '../assets/images/icon-white-10.png'
+import iconWhite11 from '../assets/images/icon-white-11.png'
+import iconWhite12 from '../assets/images/icon-white-12.png'
+import iconWhite13 from '../assets/images/icon-white-13.png'
+
+const iconWhites = [iconWhite0, iconWhite1, iconWhite2, iconWhite3, iconWhite4, iconWhite5, iconWhite6, iconWhite7, iconWhite8, iconWhite9, iconWhite10, iconWhite11, iconWhite12, iconWhite13];
+
+
+const colors = ['#ced3d6', '#ffb85d', '#6dc6ff', '#a2bee3', '#a9afb3', '#dbd491', '#80e69a', '#c0cde2', '#f4bcff', '#878d91', '#b4e4f6', '#a2ccff', '#aed6e9', '#e1e4e6'];
 
 $on(document, "DOMContentLoaded", () => {
   const cardList = new CardList({
@@ -92,6 +114,7 @@ headerComment.textContent = `오늘 날씨는 ${weatherMsg[randomBG]}입니다.`
 
 formWrapper.insertAdjacentHTML("beforeend", weatherObj[randomBG]);
 
+let currentTag = -1;
 
 weather((status, resData) => {
   console.log(resData);
@@ -111,10 +134,17 @@ weather((status, resData) => {
     weatherItemTemplateClone.onclick = () => {
       document.querySelectorAll('.weather__item--active').forEach(other => {
         other.classList.remove('weather__item--active');
+        other.style.background = '#fff';
+        if (currentTag != -1) {
+          other.querySelector('.weather__item--img').src = icons[currentTag];
+        }
       });
 
       // 날씨 태그 클릭 이벤트
       weatherItemTemplateClone.classList.toggle('weather__item--active');
+      weatherItemTemplateClone.style.background = colors[i];
+      weatherItemImg.src = iconWhites[i];
+      currentTag = i;
 
       search(i, (status, resData) => {
         const renderData = Object.values(resData);
